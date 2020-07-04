4199bafe04e39687341c7544eb8fc2a87fe41c8b
Королев Иван <idk5138@gmail.com>
2020-07-04 17:01:12 +0400

    Added js click handler

--- app-layout-demo/src/js/index.js
+++ app-layout-demo/src/js/index.js
@@ -1,11 +1,19 @@
 let controlItemList = document.getElementsByClassName("control-item");
 
-let setControlItemActive = (controlItem) => {
-    if (!controlItem.classList.contains("active")) {
-        controlItem.classList.add("active");
-    }
-    else{
+let modeTextList = ["all-voices", "speaker-mode", "stream-mode"];
 
+let setControlItemActive = (controlItem, index) => {
+    for (let currentControlItem of controlItemList) {
+        if (currentControlItem.classList.contains("active")) {
+            currentControlItem.classList.remove("active");
+        }
     }
+    controlItem.classList.add("active");
+
+    document.getElementById("active-mode").innerText = modeTextList[index];
+};
+
+for (let i = 0; i < controlItemList.length; i++) {
+    controlItemList[i].onclick = () => setControlItemActive(controlItemList[i], i);
 }
 
